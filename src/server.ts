9ac04ff6d3a42f54dc943747fa5cc81ddc692a10
f3gomes <fgomesdeluna@gmail.com>
2025-11-11 16:07:15 -0300

    feat: create first route

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,23 @@ import config from "./config";
 import cors from "cors";
 import { corsOptions } from "./config/cors";
 
+import v1Routes from "./routes/v1/index";
+
 const app = express();
 
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello API",
-  });
-});
+(async () => {
+  try {
+    app.use("/api/v1", v1Routes);
 
-app.listen(config.PORT, () => {
-  console.log(`Server is running on port http://localhost:${config.PORT}`);
-});
+    app.listen(config.PORT, () => {
+      console.log(`Server is running on port http://localhost:${config.PORT}/api/v1/`);
+    });
+  } catch (error) {
+    console.log("Failed to start the server", error);
+    if (config.NODE_ENV === "production") {
+      process.exit(1);
+    }
+  }
+})();
