d1baf7338bb8bd8b945a220e3f6c468527e57692
f3gomes <fgomesdeluna@gmail.com>
2025-11-12 12:43:36 -0300

    feat: create task routes

---
+++ src/routes/v1/task.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import taskController from "../../controllers/task.controller";
+import { validateData } from "../../middlewares/validation";
+import { taskSchema } from "../../schema/task.schema";
+
+export const taskRouter = express.Router();
+
+taskRouter.get("/task/list", taskController.findAll);
+taskRouter.post("/task/new", validateData(taskSchema), taskController.create);
+taskRouter.patch("/task/edit/:id", taskController.update);
+taskRouter.delete("/task/edit/:id", taskController.remove);
--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,23 @@ import cors from "cors";
 import { corsOptions } from "./config/cors";
 
 import v1Routes from "./routes/v1/index";
+import { taskRouter } from "./routes/v1/task.route";
 
 const app = express();
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 (async () => {
   try {
     app.use("/api/v1", v1Routes);
+    app.use("/api/v1", taskRouter);
 
     app.listen(config.PORT, () => {
-      console.log(`Server is running on port http://localhost:${config.PORT}/api/v1/`);
+      console.log(
+        `Server is running on port http://localhost:${config.PORT}/api/v1/`
+      );
     });
   } catch (error) {
     console.log("Failed to start the server", error);
